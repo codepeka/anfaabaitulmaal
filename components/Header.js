9d9fc911939f20ebaf5f5b74e1d5ee521ad619ec
anfaabaitulmaal <anfaabaitulmaalindonesia@gmail.com>
2021-09-07 13:18:41 +0700

    use tag  img

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,9 @@ export default function Header() {
 
           {/* Navbar */}
           <nav className="navbar navbar-expand-lg navbar-light px-5">
-            <a className="navbar-brand" href="#">Anfaa</a>
+            <Link href='/' passHref>
+              <a className="navbar-brand">Anfaa</a>
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
